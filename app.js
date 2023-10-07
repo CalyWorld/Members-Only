d2e053094aa22e90481c032c1ff5f031326f95a6
CalyWorld <obinnaanizoba5@gmail.com>
2023-10-07 19:09:37 +0800

    implementing layouts

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const mongoose = require("mongoose");
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
 const app = express();
 
 mongoose.set("strictQuery", false);
@@ -25,10 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
