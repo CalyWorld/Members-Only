5171befe5410695af4888ceef98426449c6922c1
CalyWorld <obinnaanizoba5@gmail.com>
2023-10-10 22:09:39 +0800

    user can change status and become a member

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,56 @@
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+require("dotenv").config();
+const membership_password = process.env.MEMBERSHIP_PASSWORD;
 exports.index = asyncHandler(async (req, res, next) => {
-  console.log({ user: req.user });
   res.render("user", {
     title: "Welcome User",
     user: req.user,
   });
 });
+exports.membership_authenticator_get = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  res.render("membership_form", {
+    title: "Membership form",
+    user: user,
+  });
+});
+exports.membership_authenticator_post = [
+  body("password", "Password must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .custom((value, { req }) => {
+      if (value !== membership_password) {
+        throw new Error("Password doesn't match");
+      }
+      return true;
+    }),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const user = await User.findById(req.params.id);
+    const userDetails = new User({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      password: user.password,
+      status: "Member",
+      _id: req.params.id,
+    });
+    console.log({ id: req.params.id, user: user });
+    if (!errors.isEmpty()) {
+      res.render("membership_form", {
+        title: "Membership form",
+        errors: errors.array(),
+      });
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        userDetails,
+        {},
+      );
+      res.redirect(updatedUser.url);
+    }
+  }),
+];
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const user_controller = require("../controllers/userController");
 
 router.get("/", user_controller.index);
+router.get("/:id/join", user_controller.membership_authenticator_get);
+router.post("/:id/join", user_controller.membership_authenticator_post);
 module.exports = router;
